d2f2cd174b68c7a4b130fb728a50f3dba135e3ed
kimkim2538 <39052613+kimkim2538@users.noreply.github.com>
2019-01-14 15:22:21 +0700

    update calculate grade

--- calculate.py
+++ calculate.py
@@ -1,17 +1,29 @@
 import csv
 
-def setup(): 
-    sumgrade = []
-    sumweight = []
-    csv = open('Transcriptonlyscore.csv', 'r')
-    data = csv.readline()
-    for i in data:
-        line_data = i.strip().split(",")
-        sumweight.append(line_data[1])
-        sumgrade.append(line_data[2])
-    csv.close()
-    print(sumgrade)
+listgrade = []
+listweight = []
 
+def setup():
+   with open('grade.csv','r') as csvfile:
+        #spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+        spamreader = csv.reader(csvfile)
+        for row in spamreader:
+           #line_data = row.strip().split(",")
+           listgrade.append(row[3])
+           listweight.append(row[1])
+           #print(','.join(row[1]))
+           #print(','.join(row[3]))
+           #print(row)
+   print (listgrade)
+   print (listweight)
+   calculate (listweight,listgrade)
 
-
+def calculate(weight,grade):
+   sum_weight = 0
+   sum_grade = 0
+   for i in range(len(grade)):
+       sum_weight += int(weight[i])
+       sum_grade += float(weight[i])*float(grade[i])
+   print ('%.2f'%(sum_grade/sum_weight))
+   
 setup()
